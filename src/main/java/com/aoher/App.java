6b57195d8b4f7035c2831293cf6764dc6073ca6d
Allega <42183540+MrsMars@users.noreply.github.com>
2020-04-27 21:00:34 +0300

    aoher | project structure organisation

---
+++ src/main/java/com/aoher/App.java
@@ -0,0 +1,22 @@
+package com.aoher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The type application
+ *
+ * @author AOher
+ */
+@SpringBootApplication
+public class App {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
